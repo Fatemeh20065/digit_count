0ca4992e0c9ce095261dd3e921e294ee08308b0d
fatemeh mardani <fatemehmardani138520065@gmail.com>
2025-12-18 10:43:30 +0330

    digit_count

---
+++ digit_count/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace digit_count
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter number");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine("digit_count : : "+digitcount(n));
+            Console.ReadKey();
+        }
+        static int digitcount(int number)
+        {
+            if (number < 10)
+            {
+                return 1;
+            }
+            else
+            {
+              return(1 + digitcount(number / 10));
+            }
+        }
+    }
+}
